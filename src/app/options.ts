eff9f5132af89b0d3030898791e7c408ba3ca6a6
jarvin-s <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-09-20 12:22:08 +0200

    fix: prisma adapter

--- src/app/options.ts
+++ src/app/options.ts
@@ -1,12 +1,13 @@
 import type { NextAuthOptions } from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
 export const options: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID!,
@@ -15,7 +16,7 @@ export const options: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-          })
+        })
     ],
     session: {
         strategy: "jwt"
@@ -23,5 +24,4 @@ export const options: NextAuthOptions = {
     jwt: {
         secret: process.env.NEXTAUTH_SECRET,
     },
-    adapter: PrismaAdapter(prisma),
 }
