41c751e6a4b03de29d21338acc346147fd8dedaf
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-09-26 00:56:31 +0200

    chore: update dashboard & navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import SignOut from '@/components/SignOut';
+import { Button } from '@/components/ui/button';
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import React from 'react'
@@ -11,12 +11,14 @@ const Dashboard = async () => {
   }
 
   return (
-    <div>
-      <div className='flex flex-row md:flex-col text-8xl text-blue-500 underline font-bold'>DASHBOARD</div>
-
-      <SignOut />
+    <div className='py-12 container relative mx-auto px-6 sm:py-16 overflow-hidden'>
+      <div className='mx-auto max-w-3xl justify-center text-center'>
+        <div className='relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8 '>
+          <h1 className='text-6xl font-bold text-left md:text-center text-scale-1200'>Experience quiz <span className='bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-yellow-800'>excellence</span> with my app!</h1> 
+          <div className='pt-4'><Button className=''>Start quiz</Button></div>
+        </div>
+      </div>
     </div>
-
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import { getServerSession } from 'next-auth'
+import SignOut from './SignOut'
 
 const Navbar = async () => {
+    const session = await getServerSession()
     return (
         <div className='fixed inset-x-0 top-0 border-b-2 py-3'>
             <div className='flex items-center justify-between h-full px-8 gap-'>
                 <Link className='flex items-center gap-2' href={'/'}>
-                    {/* <span className='text-4xl transition-all duration-300 hover:-translate-y-2'>💭</span> <span className='transition-all hover:-translate-y-2 duration-300 text-4xl hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-br hover:from-yellow-500 to-orange-200'>Gilde</span> <span className='transition-all hover:-translate-y-2 duration-300 text-4xl'>Quiz</span> */}
                     <p
                         className='rounded-lg border-2 border-b-4
                                     border-r-4 border-yellow-500 px-2 py-2 text-2xl font-bold transition-all
@@ -15,6 +17,11 @@ const Navbar = async () => {
                         💭Gilde Quiz
                     </p>
                 </Link>
+                {session?.user ? (
+                    <SignOut/>
+                ) : (
+                    <></>
+                )}
             </div>
         </div>
     )
