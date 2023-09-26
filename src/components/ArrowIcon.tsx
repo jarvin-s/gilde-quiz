ffcf2660079a2f9b59f1163ee47874c12fa0d59d
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-09-26 23:56:27 +0200

    feat: (probably temporary) added iconify

---
+++ src/components/ArrowIcon.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Icon } from '@iconify/react'
+import React from 'react'
+
+const ArrowIcon = () => {
+    return (
+        <div>
+            <Icon className='arrow-icon' icon='mdi:arrow-right-thick'/>
+        </div>
+    )
+}
+
+export default ArrowIcon
