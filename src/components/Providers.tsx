cd9e3ad9a1c376976657e80a0492d727f726cff4
jarvin-s <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-10-23 14:59:07 +0200

    chore: fix Question types & Providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 import { ThemeProvider } from './Theme/ThemeProvider'
 
--- src/types/Question.ts
+++ src/types/Question.ts
@@ -1,13 +1,6 @@
 export type QuestionsResponse = {
   // results: Question[]; // An array of trivia questions
-  question: string;
-  correct_answer: string;
-  incorrect_answers: string[];
-};
-
-type Question = {
-  question: string; // The question text
-  correct_answer: string; // The correct answer
-  incorrect_answers: string[]; // An array of incorrect answers
-  // Other properties specific to the API response
-};
+  question: string
+  correct_answer: string
+  incorrect_answers: string[]
+}
