7194ab0d1087f0b0e6f72e7252ef20496411fafa
jarvin-s <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-10-26 16:45:37 +0200

    feat: add (Quiz)Slider

---
+++ src/components/Quiz/QuizSlider.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Slider } from '../ui/slider'
+
+type SliderProps = React.ComponentProps<typeof Slider>
+
+export default function QuizSlider({className, ...props}: SliderProps) {
+
+    return (
+        <Slider
+            defaultValue={[10]}
+            max={50}
+            step={5}
+            {...props}
+            className='mt-6'
+        />
+    )
+}
