f5d93d21987a33b69c4ac9f2be6413efce1690f5
jarvin-s <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-12-21 16:59:29 +0100

    feat: added Dialog & updated dashboard
    - added: shadcn seperator/dialog
    - updated: dashboard text
    - fixed: some responsive issues

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import QuizCreateButton from '@/components/Quiz/QuizCreateButton'
+import QuizDashboard from '@/components/Quiz/QuizDashboard'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import React from 'react'
@@ -11,22 +11,9 @@ const Dashboard = async () => {
     }
 
     return (
-        <div className='container relative mx-auto overflow-hidden px-6 py-12 sm:py-16'>
-            <div className='mx-auto max-w-3xl justify-center text-center'>
-                <div className='relative z-10 flex flex-col items-center justify-center gap-4 pt-[90px] sm:mx-auto'>
-                    <h1 className='text-6xl font-bold md:text-center'>
-                        Experience quiz{' '}
-                        <span className='bg-gradient-to-r from-yellow-500 to-yellow-800 bg-clip-text text-transparent'>
-                            excellence
-                        </span>{' '}
-                        with my app!
-                    </h1>
-                    <div className='pt-4'>
-                        <QuizCreateButton/>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <QuizDashboard />
+        </>
     )
 }
 
